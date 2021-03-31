8f81622d9b741a5fde930d442382af2bf3d11636
Yakov Marushchak <ymaryshak@bk.ru>
2021-03-31 17:14:11 +0400

    Initial commit

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,48 @@
+package hiber.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String model;
+    private int series;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Car() {}
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
